b6b079d6b033eded566e3e12d31771a9f6920e8e
Ángel Arias <angadominguez96@gmail.com>
2024-10-18 21:49:51 +0200

    Terminado ej de las cervezas

--- cervezas/index.php
+++ cervezas/index.php
@@ -3,112 +3,120 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/styles.css">
   <title>Cervezas</title>
 </head>
 <body>
   <h1>Inserción de cervezas</h1>
 
+  <h4>Introduzca los datos de las Cervezas:</h4>
+
   <form action="listaCervezas.php" method="post">
-    <label for="tipoCerveza">Tipo de Cerveza: </label>
-    <select name="tipoCerveza" id="">
-      <option value="Lager">Lager</option>
-      <option value="Pale Lager">Pale Lager</option>
-      <option value="Marzen">Marzen</option>
-      <option value="Bock">Bock</option>
-      <option value="American Lager">American Lager</option>
-      <option value="Ale">Ale</option>
-      <option value="Pale Ale">Pale Ale</option>
-      <option value="IPA">IPA</option>
-      <option value="Bitter">Bitter</option>
-      <option value="Brown Ale">Brown Ale</option>
-      <option value="Porter">Porter</option>
-      <option value="Stout">Stout</option>
-      <option value="Trapenses">Trapenses</option>
-      <option value="Abadia">Abadia</option>
-      <option value="Weisbier">Weisbier</option>
-      <option value="Witbier">Witbier</option>
-    </select>
+    <div class="form-group">
+      <label for="tipoCerveza">Tipo de Cerveza: </label>
+      <select name="tipoCerveza" id="">
+        <option value="Lager">Lager</option>
+        <option value="Pale Lager">Pale Lager</option>
+        <option value="Marzen">Marzen</option>
+        <option value="Bock">Bock</option>
+        <option value="American Lager">American Lager</option>
+        <option value="Ale">Ale</option>
+        <option value="Pale Ale">Pale Ale</option>
+        <option value="IPA">IPA</option>
+        <option value="Bitter">Bitter</option>
+        <option value="Brown Ale">Brown Ale</option>
+        <option value="Porter">Porter</option>
+        <option value="Stout">Stout</option>
+        <option value="Trapenses">Trapenses</option>
+        <option value="Abadia">Abadia</option>
+        <option value="Weisbier">Weisbier</option>
+        <option value="Witbier">Witbier</option>
+      </select>
+    </div>
 
-    <br><br>
+    <div class="form-group">
+      <label for="tipoEmbase">Tipo de Embase: </label>
+      <div class="radio-group">
+        <input type="radio" name="tipo" id="tipo1" value="Botellin" checked>
+        <label for="botellin">Botellin</label>
 
-    <label for="tipoEmbase">Tipo de Embase: </label>
-    <label for="botellin">Botellin</label>
-    <input type="radio" name="tipo" id="tipo1" value="Botellin" checked>
+        <input type="radio" name="tipo" id="tipo2" value="Tercio">
+        <label for="tercio">Tercio</label>
 
-    <label for="tercio">Tercio</label>
-    <input type="radio" name="tipo" id="tipo2" value="Tercio">
+        <input type="radio" name="tipo" id="tipo3" value="1/2 litro">
+        <label for="1/2litro">1/2 Litro</label>
 
-    <label for="1/2litro">1/2 Litros</label>
-    <input type="radio" name="tipo" id="tipo3" value="1/2 litro">
+        <input type="radio" name="tipo" id="tipo4" value="Litro">
+        <label for="litro">Litro</label>
 
-    <label for="litro">Litro</label>
-    <input type="radio" name="tipo" id="tipo4" value="Litro">
+        <input type="radio" name="tipo" id="tipo5" value="Lata">
+        <label for="lata">Lata</label>
+      </div>
+    </div>
 
-    <label for="lata">Lata</label>
-    <input type="radio" name="tipo" id="tipo5" value="Lata">
+    <div class="form-group">
+      <label for="cantidadNeta">Cantidad Neta: </label>
+      <select name="cantidadNeta" id="">
+        <option value="25cl">25cl</option>
+        <option value="33cl">33cl</option>
+        <option value="1litro">1 Litro</option>
+        <option value="1/2litro">1/2 Litro</option>
+        <option value="2litro">2 Litros</option>
+        <option value="barril25litro">Barril 25 Litros</option>
+      </select>
+    </div>
 
-    <br><br>
+    <div class="form-group">
+      <label for="marca">Marca del Producto: </label>
+      <input type="text" name="marca" id="">
+    </div>
 
-    <label for="cantidadNeta">Cantidad Neta: </label>
-    <select name="cantidadNeta" id="">
-      <option value="25cl">25cl</option>
-      <option value="33cl">33cl</option>
-      <option value="1litro">1 Litro</option>
-      <option value="1/2litro">1/2 Litro</option>
-      <option value="2litro">2 Litro</option>
-      <option value="barril25litro">Barril 25 Litros</option>
-    </select>
+    <div class="form-group">
+      <label for="advertencia">Advertencia sobre el abuso en el consumo de alcohol: </label>
+      <input type="text" name="advertencia" id="">
+    </div>
 
-    <br><br>
+    <div class="form-group">
+      <label for="consumoPreferente">Fecha de Consumo Preferente: </label>
+      <input type="date" name="consumoPreferente" id="">
+    </div>
 
-    <label for="marca">Marca del Producto: </label>
-    <input type="text" name="marca" id="">
+    <div class="form-group">
+      <label for="alergenos">Indicar sustancias que pueden causar alergias: </label>
+      <div class="checkbox-group">
+        <input type="checkbox" name="aler[]" id="gluten" value="Gluten">
+        <label for="gluten">Gluten</label>
 
-    <br><br>
+        <input type="checkbox" name="aler[]" id="huevo" value="Huevo">
+        <label for="huevo">Huevo</label>
 
-    <label for="advertencia">Advertencia sobre el abuso en el consumo de alcohol: </label>
-    <input type="text" name="advertencia" id="">
+        <input type="checkbox" name="aler[]" id="cacahuete" value="Cacahuete">
+        <label for="cacahuete">Cacahuete</label>
 
-    <br><br>
+        <input type="checkbox" name="aler[]" id="soja" value="Soja">
+        <label for="soja">Soja</label>
 
-    <label for="consumoPreferente">Fecha de Consumo Preferente: </label>
-    <input type="date" name="consumoPreferente" id="">
+        <input type="checkbox" name="aler[]" id="lacteo" value="Lacteo">
+        <label for="lacteo">Lacteo</label>
 
-    <br><br>
+        <input type="checkbox" name="aler[]" id="sulfitos" value="Sulfitos">
+        <label for="sulfitos">Sulfitos</label>
 
-    <label for="alergenos">Indicar sustancias que pueden causar alergias: </label>
-    <label for="gluten">Gluten</label>
-    <input type="checkbox" name="aler" id="gluten" value="Gluten" checked>
+        <input type="checkbox" name="aler[]" id="fcascara" value="Frutos con Cascara">
+        <label for="fcascara">Frutos Con Cascara</label>
 
-    <label for="huevo">Huevo</label>
-    <input type="checkbox" name="aler" id="huevo" value="Huevo">
+        <input type="checkbox" name="aler[]" id="sinAlergenos" value="Sin Alergenos" checked>
+        <label for="sinAlergenos">Sin Alergenos</label>
+      </div>
+    </div>
 
-    <label for="cacahuete">Cacahuete</label>
-    <input type="checkbox" name="aler" id="cacahuete" value="Cacahuete">
-
-    <label for="soja">Soja</label>
-    <input type="checkbox" name="aler" id="soja" value="Soja">
-
-    <label for="lacteo">Lacteo</label>
-    <input type="checkbox" name="aler" id="lacteo" value="Lacteo">
-
-    <label for="sulfitos">Sulfitos</label>
-    <input type="checkbox" name="aler" id="sulfitos" value="Sulfitos">
-
-    <label for="fcascara">Frutos Con Cascara</label>
-    <input type="checkbox" name="aler" id="fcascara" value="Frutos con Cascara">
-
-    <label for="sinAlergenos">Sin Alergenos</label>
-    <input type="checkbox" name="aler" id="sinAlergenos" value="Sin Alergenos">
-
-    <br><br>
-
-    <label for="observaciones">Observaciones: </label>
-    <textarea name="observaciones" id=""></textarea>
-
-    <br><br><br>
+    <div class="form-group">
+      <label for="observaciones">Observaciones: </label>
+      <textarea name="observaciones" id=""></textarea>
+    </div>
 
     <input type="submit" value="Insertar Cerveza">
   </form>
 </body>
+
 </html>
--- cervezas/listaCervezas.php
+++ cervezas/listaCervezas.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="./styles/styles.css">
   <title>Lista de Cervezas</title>
 </head>
 <body>
@@ -17,34 +18,40 @@
     $alergenos = $_REQUEST['aler'];
     $observaciones = $_REQUEST['observaciones'];
 
-    if(!isset($marca) || !isset($advertencia) || !isset($consumoPreferente) || !isset($alergenos)) {
+    if(empty($marca) || empty($advertencia) || empty($consumoPreferente) || empty($alergenos)) {
       ?>
-      <h3>No se ha podido realizar la inserción debido a los siguentes errores: </h3>
-      <ul>
-        <li>Se requiere Marca</li>
-        <li>Es obligatorio advertencia sobre el consumo de alcohol</li>
-        <li>No ha introducido fecha</li>
-        <li>Es obligatorio incluir alergenos</li>
-      </ul>
+      <div class="error-group">
+        <h3>No se ha podido realizar la inserción debido a los siguentes errores: </h3>
+        <ul>
+          <li>Se requiere Marca</li>
+          <li>Es obligatorio advertencia sobre el consumo de alcohol</li>
+          <li>No ha introducido fecha</li>
+          <li>Es obligatorio incluir alergenos</li>
+        </ul>
+  
+        <br><br>
+        <a href="index.php">[ Volver ]</a>
+      </div>
       <?php
     } else {
       ?>
-        <ul>
-          <li>Tipo de Cerveza: <?php echo $tipoCerveza ?></li>
-          <li>Tipo Embase: <?php echo $tipo ?> </li>
-          <li>Cantidad Neta: <?php echo $cantidadNeta ?></li>
-          <li>Marca del producto: <?php echo $marca ?></li>
-          <li>Advertencia: <?php echo $advertencia ?></li>
-          <li>Fecha de consumo preferente: <?php echo $consumoPreferente ?></li>
-          <li>Alergenos: <?php echo $alergenos ?></li>
-          <li>Observaciones: <?php echo $observaciones ?></li>
-        </ul>
+        <div class="error-group">
+          <ul>
+            <li>Tipo de Cerveza: <?php echo $tipoCerveza ?></li>
+            <li>Tipo Embase: <?php echo $tipo ?> </li>
+            <li>Cantidad Neta: <?php echo $cantidadNeta ?></li>
+            <li>Marca del producto: <?php echo $marca ?></li>
+            <li>Advertencia: <?php echo $advertencia ?></li>
+            <li>Fecha de consumo preferente: <?php echo $consumoPreferente ?></li>
+            <li>Alergenos: <?php echo implode(' - ', $alergenos) ?></li>
+            <li>Observaciones: <?php echo $observaciones ?></li>
+          </ul>
+          
+          <br><br>
+          <a href="index.php">[ Insertar otra ]</a>
+        </div>
       <?php
     }
   ?>
-
-  
-
-  <a href="index.php">Volver</a>
 </body>
 </html>
