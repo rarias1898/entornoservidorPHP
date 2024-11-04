30a86fc08fdc8054213170c650516c1118132fd8
Ángel Arias <angadominguez96@gmail.com>
2024-11-04 02:39:08 +0100

    añadidos ejercicios 1 y 2

---
+++ boletin2/Ejercicio_2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Ejercicio 2</title>
+</head>
+<body>
+  <!-- Hacer un programa hacer que realice una conversión entre diferentes tipos de datos en
+PHP.
+Crear una variable $var con los valores que se observa en la tabla adjunta y para cada valor
+de la variable $var, pintar por pantalla la conversión de cada valor a los siguientes tipos:
+int, boolean, string y float  -->
+</body>
+</html>
