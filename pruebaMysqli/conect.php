3ee968bbe151d6855668367be1e8b113520a6e19
rarias1898 <rarias1898@ceuandalucia.es>
2024-11-13 20:02:55 +0100

    Actualizacion conexion mysqli

--- pruebaMysqli/conect.php
+++ pruebaMysqli/conect.php
@@ -1,46 +1,27 @@
 <?php
-// MySQLi connection
-<<<<<<< HEAD
-try {
-    $mysqli = new mysqli("127.0.0.1", "root", "root", "hola");
-} catch (mysqli_sql_exception $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-=======
-$mysqli = new mysqli("127.0.0.1", "root", "", "prueba");
->>>>>>> e1a3d1939d87a38e303809be72e7444b69d088b8
+$mysql = new mysqli("127.0.0.1:3307", "root", "root", "prueba");
 
-// Check connection
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
+if ($mysql->connect_error) {
+    die("Connection failed: " . $mysql->connect_error);
 }
 
-// SQL to select from table
-<<<<<<< HEAD
-$sql = "SELECT * FROM user";
-=======
 $sql = "SELECT * FROM users";
->>>>>>> e1a3d1939d87a38e303809be72e7444b69d088b8
 
-// Execute query
-$result = $mysqli->query($sql);
+$result = $mysql->query($sql);
 
-// Check if the query was successful
 if ($result) {
-    // Fetch data and display
     if ($result->num_rows > 0) {
-        // Output data of each row
         while ($row = $result->fetch_assoc()) {
-            echo "ID: " . $row["id"] . " - Name: " . $row["nombre"] . "<br>";
+            // var_dump("ID: " . $row["id"] . " - Name: " . $row["name"] . " - Surname: " . $row["surname"]);
+            print "Bienvenido" . " " . $row["name"];
         }
     } else {
         echo "No results found";
     }
 } else {
-    echo "Error executing query: " . $mysqli->error;
+    var_dump("Error executing query: " . $mysql->error);
 }
 
 
-// Close connection
-$mysqli->close();
+$mysql->close();
 ?>
