c32717dcf8d3e5e23aa0f8f0ad523993248e629c
Ángel Arias <angadominguez96@gmail.com>
2024-11-19 23:21:51 +0100

    Añadir validación de DNI y verificación de existencia de usuario en el proceso de registro

--- ExamenPrimerTrimestre/Ejercicio_3/register_process.php
+++ ExamenPrimerTrimestre/Ejercicio_3/register_process.php
@@ -8,12 +8,27 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $usuario = mysqli_real_escape_string($connection, $_POST['usuario']);
     $contraseña = mysqli_real_escape_string($connection, $_POST['password']);
 
+    $comprobar_usuario = "SELECT * FROM usuarios WHERE DNI = '$dni' OR Usuario = '$usuario'";
+    $result_comprobar = $connection->query($comprobar_usuario);
+
+    if (!preg_match("/^[0-9]{8}[A-Z]$/", $dni)) {
+        echo "El DNI no es correcto";
+        print "<br><br><a href='register.php'>Volver</a>";
+        exit();
+    }
+
+    if ($result_comprobar->num_rows > 0) {
+        echo "El usuario ya existe";
+        print "<br><br><a href='register.php'>Volver</a>";
+        exit();
+    }
+
     $query = "INSERT INTO usuarios (DNI, Nombre, Apellidos, Usuario, Contraseña) VALUES ('$dni', '$nombre', '$apellidos', '$usuario', '$contraseña')";
     
     if ($connection->query($query) === TRUE) {
         header("Location: index.php");
     } else {
-        echo "Error: " . $conn->error;
+        echo "Error: " . $connection->error;
     }
     $connection->close();
 }
