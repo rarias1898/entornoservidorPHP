8b59a3a213903a1daa9e7df43171dfec672aa741
aaridoms <angadominguez96@gmail.com>
2025-01-22 13:46:48 +0100

    empezado segunda parte de las cervezas

---
+++ cervezas2/index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Cervecería</title>
+    <link rel="stylesheet" href="./styles/index.css">
+  </head>
+  <body>
+    <header>
+      <img src="./images/logo.png" alt="">
+      <div class="button-group">
+        <button>Login</button>
+        <button>Signup</button>
+      </div>
+    </header>
+    <main>
+      <h1>Aquí no vendemos Cruzcampo!</h1>
+    </main>
+    <footer>
+      <h2>Este es el footer</h2>
+      <hr>
+      <small>2025 Entorno servidor | Ángel Arias</small>
+    </footer>
+  </body>
+</html>
